dc935768bee6613f1b560fda1c5c087fca52ecdd
Jia <cwu005104@gmail.com>
2026-02-19 23:55:21 +0800

    feat: set gh-pages r2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  homepage:"https://jiawu777.github.io/hex-week5-hw/"
+  homepage:"https://jiawu777.github.io/hex-week5-hw"
 })
